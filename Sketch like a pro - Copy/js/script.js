3da7c25bad7038f2258b62b80fac4b69cecdf409
Michele Zucca <michele.zucca81@gmail.com>
2019-11-20 11:02:15 +0200

    removed bootstrap from sketch like a pro

--- Sketch like a pro - Copy/js/script.js
+++ Sketch like a pro - Copy/js/script.js
@@ -1,9 +1,36 @@
+
+
+//NAVBAR TOGGLER
+
+
+let hamburgerBar = document.querySelector('.hamburger-bar');
+let navLinks = document.querySelector('.navbar-links');
+let links = document.querySelectorAll('.navbar-links li');
+let navbarClose = document.getElementById('exit');
+
+
+
+hamburgerBar.addEventListener('click', function(e){
+    console.log('working');
+    navLinks.classList.toggle("open");
+    hamburgerBar.classList.add("hide");
+    e.preventDefault();
+});
+
+navbarClose.addEventListener('click', function(e) {
+    console.log('exiting nav')
+    navLinks.classList.toggle("open");
+    hamburgerBar.classList.remove('hide');
+    e.preventDefault();
+});
+
 //Timeline header animation
+
 const navBar = document.querySelector('.navbar');
 const imageContainer = document.querySelector('.container-image');
 const slider = document.querySelector('.slider-container');
 const logo = document.querySelector('#logo');
-const navItem = document.querySelectorAll('.nav-item');
+const navItem = document.querySelectorAll('.item');
 const liquid = document.querySelector('.liquid-image');
 const firstHeader = document.querySelector('.header-primary');
 const subHeader = document.querySelector('.subheading');
@@ -21,7 +48,6 @@ timeline.fromTo(imageContainer, 1, {height: '0%'}, {height: '90%', ease:Power2.e
 .fromTo(firstHeader, 0.5, {opacity: 0, x:30}, {opacity: 1, x: 0}, "-=0.5")
 .fromTo(subHeader, 0.5, {opacity: 0, x:30}, {opacity: 1, x: 0}, "-=0.5")
 .fromTo(checkBtn, 0.5, {opacity: 0, x:30}, {opacity: 1, x: 0}, "-=0.5");
-
 //LIQUID IMAGE
 
 new hoverEffect({
--- eat.taste.try_Sass/js/main.js
+++ eat.taste.try_Sass/js/main.js
@@ -8,9 +8,20 @@ ScrollReveal().reveal('.punchline', { delay: 1000})
 var hamburger = document.querySelector('.navbar__hamburger');
 var navLinks = document.querySelector('.navbar__nav-links');
 var links = document.querySelectorAll('.navbar__nav-links li');
+var lineOne = document.getElementById('navbar__line_1');
+var lineTwo = document.getElementById('navbar__line_2');
+const upperLine = document.getElementById('upper-line');
+const lowerLine = document.getElementById('lower-line');
+
 
 hamburger.addEventListener('click', function (){
     navLinks.classList.toggle("open");
+    lineOne.classList.toggle('rotate-1');
+    lineTwo.classList.toggle('rotate-3');
+    hamburger.classList.toggle('add-margin');
+    upperLine.classList.toggle('make-transparent');
+    lowerLine.classList.toggle('make-transparent');
+
 });
 
 //Back to top button
