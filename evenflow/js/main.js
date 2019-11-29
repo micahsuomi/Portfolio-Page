386124c6ce17d423154ae41c981b3586b6d40a4e
Michele Zucca <michele.zucca81@gmail.com>
2019-11-29 18:18:10 +0200

    modified all files and css, grid, styles

--- evenflow/js/main.js
+++ evenflow/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
@@ -6,13 +5,22 @@ const links = document.querySelectorAll('.nav-links li');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
-=======
-
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
->>>>>>> df05c7919672b27bb7c1b546138851bfbce8338d
 });
+
+const reviewImages = document.querySelector('.review-images');
+const reviewImg = document.querySelector('.postbox-bigimg');
+const reviewImgHidden = document.querySelector('.postbox-bigimg-hidden');
+
+
+
+reviewImages.addEventListener('mouseover', () => {
+    reviewImg.classList.add('hide');
+    reviewImgHidden.classList.add('show');
+
+})
+
+reviewImages.addEventListener('mouseout', () => {
+    reviewImg.classList.remove('hide');
+    reviewImgHidden.classList.remove('show');
+
+})
