73d67698c1ba50fbb2f86fd63493657d2449d7ed
Michele Zucca <michele.zucca81@gmail.com>
2019-12-01 01:28:25 +0200

    adjusted responsivness, added normalize.css

--- evenflow/js/main.js
+++ evenflow/js/main.js
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
-
+AOS.init({
+    offset: 400,
+    duration: 800
+  });
+  
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
-=======
-
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
->>>>>>> df05c7919672b27bb7c1b546138851bfbce8338d
 });
